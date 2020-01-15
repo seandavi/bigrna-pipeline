5bf3440d8dcedb6b1300789edde4b33610d2cec4
Sean Davis <seandavi@gmail.com>
2020-01-15 15:43:18 -0500

    trying max_lease_duration setting.

--- bigrna_pipeline/pubsub.py
+++ bigrna_pipeline/pubsub.py
@@ -14,9 +14,6 @@ def make_command_line(experiment: str, index: str, gtf: str):
     nf_loc = pkg_resources.resource_filename('bigrna_pipeline', 'main.nf')
     return f'./nextflow run {nf_loc} --experiment {experiment} --with-trace --with-report report.html --index {index} --gtf {gtf} --transcript_version v32'.split()
 
-
-# Define the callback.
-# Note that the callback is defined *before* the subscription is opened.
 def callback(message):
     print(message)  # Replace this with your actual logic.
     idx = 'gencode.v32.all_transcripts.k31'
@@ -24,22 +21,11 @@ def callback(message):
     z = make_command_line(message.data.decode('UTF-8'), idx, gtf)
     logging.info(f'got new message {message}')
     try:
-        proc = subprocess.Popen(z)
-        while True:
-            res = proc.poll()
-            if(res is None):
-                time.sleep(60)
-                message.modify_ack_deadline(120)
-                continue
-            if(res == 0):
-                message.ack()  # Asynchronously acknowledge the message.
-                break
-            else:
-                message.nack()
-                logging.error('message {message} returned non-zero return code')
+        subprocess.run(z)
+        message.ack()
     except Exception:
         logging.error(f'missed message {message}')
-        message.nack()
+        message.ack()
 
     # cleanup directory
     subprocess.run('rm -rf work', shell=True)
@@ -49,7 +35,7 @@ def callback(message):
 subscription_path = subscriber.subscription_path(PROJECT, SUBSCRIPTION)
 
 # Open the subscription, passing the callback.
-fc = pubsub.types.FlowControl(max_messages=1)
+fc = pubsub.types.FlowControl(max_messages=1, max_lease_duration=18000)
 logging.info(f'awaiting messages')
 future = subscriber.subscribe(subscription_path, callback, flow_control=fc)
 
