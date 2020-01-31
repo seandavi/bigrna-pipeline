423b3f8346309e81f2942b6ec2d1e70dbae933ff
Sean Davis <seandavi@gmail.com>
2020-01-31 16:49:32 -0500

    major change to use synchronous pull
    Asynchronous pulling (using futures and "subscribe") uses
    a buffer in the gRPC stream. So, messages enter that buffer
    and stay there. When we have long-running processes with each
    message and many subscribers, messages can appear to be "blocked"
    in the pubsub queue. The way around this is to handle all the
    pubsub pulling synchronously. This also necessitates resetting the
    ack deadline regularly. All good, though, and actually more
    transparent.

--- bigrna_pipeline/pubsub.py
+++ bigrna_pipeline/pubsub.py
@@ -1,17 +1,20 @@
-from google.cloud import pubsub
+from google.cloud import pubsub_v1
 
 import subprocess
+import shlex
 import logging
 import traceback
 import sys
 import json
 import os
+import time
 
 import pkg_resources
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-subscriber = pubsub.SubscriberClient()
+subscriber = pubsub_v1.SubscriberClient()
+
 
 class Config():
     DATABASE_URL = os.environ.get('DATABASE_URL')
@@ -19,41 +22,59 @@ class Config():
     PUBSUB_SUBSCRIPTION = os.environ.get("PUBSUB_SUBSCRIPTION")
     PUBSUB_TOPIC = os.environ.get("PUBSUB_TOPIC")
 
+def get_subscription_path():
+    return subscriber.subscription_path(Config.PROJECT_ID, Config.PUBSUB_SUBSCRIPTION)
+
+
+    
 def make_command_line(experiment: str, run_id: str, index: str, gtf: str):
     nf_loc = pkg_resources.resource_filename('bigrna_pipeline', 'main.nf')
-    return f'./nextflow run {nf_loc} --run_id {run_id} --experiment {experiment} -with-trace -with-report report.html --index {index} --gtf {gtf} --transcript_version v32'.split()
+    #return shlex.split(f'./nextflow run {nf_loc} --run_id {run_id} --experiment {experiment} -with-trace -with-report report.html --index {index} --gtf {gtf} --transcript_version v32')
+    return(['sleep','20'])
 
-def callback(message):
+def start_nextflow_subprocess(message) -> subprocess.Popen:
     logging.info('handling message %s', message)
     dat = json.loads(message.data.decode('UTF-8'))
     idx = 'gencode.v32.all_transcripts.k31'
     gtf = 'gencode.v32.annotation.gtf.gz'
     z = make_command_line(dat['accession'], dat['run'], idx, gtf)
-    logging.debug(f'running command "{z}"')
-    try:
-        proc_result = subprocess.run(z, capture_output = True, check = True)
-        if(proc_result.returncode==0): #successful
-            logging.info(f'successfully completed {message}')
-            with open('success.txt', 'w') as f:
-                f.writelines(proc_result.stdout.decode('UTF-8'))
-    except:
-        logging.exception(f'{message} failed with exit code {proc_result.returncode}')
-        with open('failed.txt', 'w') as f:
-            f.writelines(proc_result.stderr.decode('UTF-8'))
-        
-        
+    return subprocess.Popen(z, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+
+def get_next_message() -> pubsub_v1.types.message:
+    subscription_path = get_subscription_path()
+    response = subscriber.pull(subscription_path, max_messages = 1)
+    # should be only one message
+    for r in response.received_messages:
+        message = r
+    return message
+
+
+def write_std_files(process: subprocess.Popen):
+    # TODO: finish this....
     with open('process_stdout.txt', 'w') as f:
-        f.writelines(proc_result.stdout.decode('UTF-8'))
+        f.writelines(process.stdout.decode('UTF-8'))
     sys.stdout.writelines(proc_result.stdout.decode('UTF-8'))
     with open('process_stderr.txt', 'w') as f:
-        f.writelines(proc_result.stderr.decode('UTF-8'))
-    sys.stderr.writelines(proc_result.stderr.decode('UTF-8'))
-    # we are going to ack everything
-    message.ack()
-    logging.debug(f'generated command-line {z}')
+        f.writelines(process.stderr.decode('UTF-8'))
+    sys.stderr.writelines(process.stderr.decode('UTF-8'))
+    
 
+def failed_pipeline(process: subprocess.Popen, message: pubsub_v1.types.message):
+    logging.exception(f'{message} failed with exit code {process.poll()}')
+    with open('failed.txt', 'w') as f:
+        f.write(f'{message} failed with exit code {process.poll()}')
+    
+
+def succeeded_pipeline(process: subprocess.Popen, message: pubsub_v1.types.message):
+    logging.exception(f'{message} succeeded with exit code {process.poll()}')
+    with open('success.txt', 'w') as f:
+        f.write(f'{message} succeeded with exit code {process.poll()}')
+    
+
+def cleanup():
     # cleanup directory
-    logging.info('copying trace and report files')
+    logging.info('cleaning up trace and report files')
     files_to_capture = [
         'success.txt',
         'failed.txt',
@@ -63,27 +84,36 @@ def callback(message):
         'report.html'
     ]
     for fname in files_to_capture:
-        try:
-            subprocess.run(f'gsutil -h x-goog-meta-bigrna-run:{dat["run"]} cp {fname} gs://bigrna-cancerdatasci-org/v2/{dat["run"]}/{fname}', shell=True)
-        except:
-            pass
-    try:
-        subprocess.run('rm -rf work', shell=True)
-    except:
-        pass
-    try:
-        subprocess.run('rm '+' '.join(files_to_capture), shell=True)
-    except:
-        pass
+        subprocess.run(f'gsutil -h x-goog-meta-bigrna-run:{dat["run"]} cp {fname} gs://bigrna-cancerdatasci-org/v2/{dat["run"]}/{fname}', shell=True)
+    subprocess.run('rm -rf work', shell=True)
+    subprocess.run('rm '+' '.join(files_to_capture), shell=True)
+        
     
+def run_to_death():
+    subscription_path = get_subscription_path()
+    while True:
+        message = get_next_message()
+        print(message)
+        process = start_nextflow_subprocess(message.message)
+        while process.poll() is None:
+            subscriber.modify_ack_deadline(
+                subscription_path,
+                [message.ack_id],
+                ack_deadline_seconds=480,
+            )
+            time.sleep(5)
+            sys.stderr.write('.')
+            sys.stderr.flush()
+        if process.poll() == 0:
+            succeeded_pipeline(process, message.message)
+        else:
+            failed_pipeline(process, message.message)
+        subscriber.acknowledge(subscription_path, [message.ack_id])
+        logging.info(f'acked {message.message}')
+        cleanup()
+
 # Substitute PROJECT and SUBSCRIPTION with appropriate values for your
 # application.
-subscription_path = subscriber.subscription_path(Config.PROJECT_ID, Config.PUBSUB_SUBSCRIPTION)
-
-# Open the subscription, passing the callback.
-fc = pubsub.types.FlowControl(max_messages=1, max_lease_duration=7*24*60*60) #7 days
-logging.info(f'awaiting messages')
-future = subscriber.subscribe(subscription_path, callback, flow_control=fc)
 
 if __name__ == '__main__':
-    future.result()
+    run_to_death()
